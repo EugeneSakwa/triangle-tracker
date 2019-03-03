c2ba623f8b3ce2fdd3ab2055452ddd74870b28f7
Eugene Sakwa <eugenesakwa@gmail.com>
2019-03-03 21:44:31 +0300

    changes the triangle checker function to remove redundant code

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
-function resultCheck() {
-	// body...
-	var values = [a,b,c];
-	var values[0] = document.querySelector('[name="a"]').value;
-	var values[1] = document.querySelector('[name="b"]').value;
-	var values[2] = document.querySelector('[name="c"]').value;
-	// display values of a, b and c as provided in the console
-	a = values[0];
-	b = values[1];
-	c = values[2];
-	console.log(a,b,c);
-	// check if the value is greater than zero
-	if ((a+b)>c && (a+c) >b && (b+c) > a){
-		if (true) {
-			if (a <= 0 || b == 0 || c == 0) {
-				alert("Not a triangle.");
-			}
-			else if (a == b && a == c && b == c) {
-				alert("Triangle is equilateral.");
-			}
-			else if (a == b || b == c || a == c)  {
-				alert("Triangle is isosceles.");
-			}
-			else(a != b && b != c && c != a) {
-				alert("Triangle is scalene.");
-			}
-		}
-	}
-	else{
-		alert("Triangle cannot be formed");
-	}
+function triangle() {
+	// get the values of triangle sides from the form
+  var a=document.getElementById("length").value;
+  var b=document.getElementById("width").value;
+  var c=document.getElementById("height").value;
+	// convert the values to integers
+  var arrays=[parseInt(a),parseInt(b),parseInt(c)];
+	// check if a triangle can be formed
+  if (arrays[0]+arrays[1]>arrays[2] &&arrays[0]+arrays[2]>arrays[1] &&arrays[1]+arrays[2]>arrays[0]){
+		// determine the type of triangle that will be formed
+		if (arrays[0]==arrays[1] && arrays[0]==arrays[2]){
+      alert('Equilateral Triangle');
+    }else if (arrays[0]==arrays[1] || arrays[0]==arrays[2] || arrays[1]==arrays[2]) {
+      alert('Isosceles Triangle');
+    }else{
+      alert('Scalene Triangle');
+    }
+  }else {
+    alert('Triangle Cannot Be Formed');
+  }
 }
